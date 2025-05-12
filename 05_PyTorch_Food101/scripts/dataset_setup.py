58753ac47e8f402104b6861c4761986ef2017853
ThomasPierantozzi <thomas.pierantozzi@gmail.com>
2025-05-12 13:18:02 +0200

    Added a TransferLearning_with_augmentation file in section 6 to test the final result

--- 05_PyTorch_Food101/scripts/dataset_setup.py
+++ 05_PyTorch_Food101/scripts/dataset_setup.py
@@ -18,7 +18,7 @@ def download_Food101_dataset(path: pathlib.Path, logger: utilities.Logging_Agent
         Food101(root=path, download=True)
         logger.write_log(f'The Food101 dataset has been downloaded and is available @ {path}')
         shutil.copytree(path / 'food-101/images', path / 'images')
-        shutil.rmtree(path / 'food-101')
+        shutil.rmtree(path / 'food-101') #bug corrected here
         os.remove(path / 'food-101.tar.gz')
     else:
         logger.write_log(f'The Food101 dataset already exists @ {path}')
@@ -32,12 +32,7 @@ def pick_foods(food101_folder, logger: utilities.Logging_Agent, number_of_foods:
     :param logger: a logging agent defined by means of the utilities script
     :return: picked_food in a list of 'number_of_foods' length
     '''
-    if food101_folder.exists():
-        logger.write_log('The food101 folder exist...')
-    else:
-        logger.write_log('The food101 folder does not exist...The download will start soon...')
-        download_Food101_dataset(path=food101_folder, logger=logger)
-    
+
     #we pick 5 dishes amongst the ones in the original dataset
     food_list = [] #a list to append the name of the different dishes
     for (path, subdirs, files) in os.walk(food101_folder):
@@ -113,6 +108,13 @@ def dataset_creation(food101_folder: pathlib.Path,
         logger.write_log(f'The folders have been created:\n'
               f'\t{dataset_train_folder}'
               f'\n\t{dataset_test_folder}')
+
+        if food101_folder.exists():
+            logger.write_log('The food101 folder exist...')
+        else:
+            logger.write_log('The food101 folder does not exist...The download will start soon...')
+            download_Food101_dataset(path=food101_folder, logger=logger)
+
         if 'picked_foods' in kwargs:
             picked_foods = kwargs['picked_foods']
         else:
