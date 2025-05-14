Fixed a tensor dimension and softmax applied to the prediction before having it output from off the eval_step

05_PyTorch_Food101/scripts/model_setup.py
@@ -180,7 +180,7 @@ class Model_Blueprint(nn.Module, ABC):
             acc_batch_cv = self.accuracy(y_hat_cv, batch_test_y)
 
             if 'return_pred' in kwargs:
-                prediction = torch.argmax(nn.Softmax(y_hat_cv), dim=1)
+                prediction = torch.argmax(nn.functional.softmax(y_hat_cv, dim=1), dim=1)
                 return loss_batch_cv.item(), acc_batch_cv, prediction
 
             return loss_batch_cv.item(), acc_batch_cv